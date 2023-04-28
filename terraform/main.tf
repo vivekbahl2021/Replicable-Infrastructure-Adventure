203fb79d0cbadb1c85b29a44380b573635da565f
David Del Sol <daviddelsol1998@gmail.com>
2023-04-28 15:34:58 -0500

    feat: remove test RG

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,2 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-test-adevnture-eastus-01"
-  location = "eastus"
-  tags = {
-    Environment             = "test"
-    Deployed_with_Terraform = "true"
-  }
-}
+
 
