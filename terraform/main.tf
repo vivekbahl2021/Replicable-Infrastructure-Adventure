742ba1ff609a8f6f518c76cf0e36645348933d9d
vivekbahl2021 <36824751+vivekbahl2021@users.noreply.github.com>
2023-07-04 11:00:30 -0400

    added a main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,2 +1,16 @@
+resource "azurerm_resource_group" "rg" {
 
+   name     = "rg-test-adevnture-eastus-01"
+
+  location = "eastus"
+
+   tags = {
+
+     Environment             = "test"
+
+     Deployed_with_Terraform = "true"
+
+   }
+
+}
 
